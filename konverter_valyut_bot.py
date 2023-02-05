bbb0af29f5020029f3eb9bac2358d16236e3f7ed
Irina Fisunova <irina.l.fisunova@gmail.com>
2023-02-05 14:55:40 +0300

    Написание кода

--- konverter_valyut_bot.py
+++ konverter_valyut_bot.py
@@ -12,8 +12,24 @@ keys = {
     'Иена':'JPY'
 }
 
-@bot.message_handler()
-def echo_test(message: telebot.types.Message):
-    bot.send_message(message.chat.id, 'Hello')
+#@bot.message_handler()
+#def echo_test(message: telebot.types.Message):
+#    bot.send_message(message.chat.id, 'Hello')
+
+@bot.message_handler(commands=['начать', 'помощь'])
+def hepl(message: telebot.types.Message):
+    text = "Чтобы начать работу введите команду боту в следующем формате: \n <имя валюты> \
+            <в какую валюту перевести> \
+            <количество переводимой валюты>"
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['валюта'])
+def values(message: telebot.types.Message):
+    text = "Доступные валюты"
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+
 
 bot.polling()
