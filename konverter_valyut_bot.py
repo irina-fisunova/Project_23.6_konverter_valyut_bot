874798ce9b77274b1ef0e00e41a511bc14137b1d
Irina Fisunova <irina.l.fisunova@gmail.com>
2023-02-05 16:13:45 +0300

    Написание кода

--- konverter_valyut_bot.py
+++ konverter_valyut_bot.py
@@ -1,5 +1,6 @@
 import json
 
+import requests
 import telebot
 
 TOKEN = '6103009775:AAE9yL2U_uCW-Fvk5zwujKRTjxtsvwGfcNo'
@@ -7,16 +8,16 @@ TOKEN = '6103009775:AAE9yL2U_uCW-Fvk5zwujKRTjxtsvwGfcNo'
 bot = telebot.TeleBot(TOKEN)
 
 keys = {
-    'Рубль':'RUB',
-    'Доллар':'USD',
-    'Евро':'EUR'
+    'рубль':'RUB',
+    'доллар':'USD',
+    'евро':'EUR'
 }
 
 #@bot.message_handler()
 #def echo_test(message: telebot.types.Message):
 #    bot.send_message(message.chat.id, 'Hello')
 
-@bot.message_handler(commands=['/start ', '/help'])
+@bot.message_handler(commands=['start', 'help'])
 def hepl(message: telebot.types.Message):
     text = "Чтобы начать работу введите команду боту в следующем формате: \n <имя валюты цену которой хотите узнать> \
     <имя валюты в которой надо узнать цену первой валюты> \
@@ -30,12 +31,13 @@ def values(message: telebot.types.Message):
         text = '\n'.join((text, key, ))
     bot.reply_to(message, text)
 
-@bot.message_handler(commands=['text', ])
+@bot.message_handler(content_types=['text',])
 def convert(message: telebot.types.Message):
-    доллар рубль 1
-    quote, base, amount = message.text.splite('')
-    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote}&tsyms={base}')
-    text = json.loads(r.content)[base]
+    quote, base, amount = message.text.split(' ')
+    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}')
+    text = round((float(json.loads(r.content)[keys[base]]) * float(amount)),2)
+    bot.send_message(message.chat.id, text)
+
 
 
 bot.polling()
