3f9376d5c86f90c5e417fd189808cee4342dbd4d
Irina Fisunova <irina.l.fisunova@gmail.com>
2023-02-06 12:21:43 +0300

    Написание кода

--- konverter_valyut_bot.py
+++ konverter_valyut_bot.py
@@ -1,6 +1,7 @@
 import json
 
 import requests
+
 import telebot
 
 TOKEN = '6103009775:AAE9yL2U_uCW-Fvk5zwujKRTjxtsvwGfcNo'
@@ -13,14 +14,44 @@ keys = {
     'евро':'EUR'
 }
 
+
 class ConvertionException(Exception):
     pass
+
+
+class CryptoConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+       if quote == base:
+            raise ConvertionException(f"Невозможно перевести одинаковые валюты {base}.")
+
+       try:
+            quote_ticker = keys[quote]
+       except KeyError:
+            raise ConvertionException(f"Не удалось обработать валюту {quote}")
+
+       try:
+            base_ticker = keys[quote]
+       except KeyError:
+            raise ConvertionException(f"Не удалось обработать валюту {base}")
+
+       try:
+            amount = float(amount)
+       except ValueError:
+            raise ConvertionException(f"Не удалось обработать количество {amount}.")
+
+       r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+       total_base = json.loads(r.content)[keys[base]]
+
+       return total_base
+# total_base = str(round((float(json.loads(r.content)[keys[base]]) * float(amount)), 2))
+
 
 @bot.message_handler(commands=['start', 'help'])
 def hepl(message: telebot.types.Message):
     text = "Чтобы начать работу введите команду боту в следующем формате: \n <имя валюты цену которой хотите узнать> \
     <имя валюты в которой надо узнать цену первой валюты> \
-    <количество первой валюты> \n Увидеть список всех возможных влют: /валюта"
+    <количество первой валюты> \n Увидеть список всех возможных влют: /values"
     bot.reply_to(message, text)
 
 @bot.message_handler(commands=['values'])
@@ -34,34 +65,15 @@ def values(message: telebot.types.Message):
 def convert(message: telebot.types.Message):
     values = message.text.split(' ')
 
-    if len(values) > 3:
+    if len(values) != 3:
         raise ConvertionException("Слишком много параметров.")
 
     quote, base, amount = values
+    total_base = CryptoConverter.convert(quote, base, amount)
 
-    if quote == base:
-        raise ConvertionException(f"Невозможно перевести одинаковые валюты {quote}.")
-
-    try:
-        quote_ticker = keys[quote]
-    except KeyError:
-        raise ConvertionException(f"Не удалось обработать валюту {quote}")
-    try:
-        base_ticker = keys[quote]
-    except KeyError:
-        raise ConvertionException(f"Не удалось обработать валюту {base}")
-    try:
-        amount = float(amount)
-    except ValueError:
-        raise ConvertionException(f"Не удалось обработать количество {amount}.")
-
-    quote_ticker, base_ticker = keys[quote], keys[base]
-
-    r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-    total_base = str(round((float(json.loads(r.content)[keys[base]]) * float(amount)),2))
     text = f"Цена {amount} {quote} в {base} - {total_base}"
     bot.send_message(message.chat.id, text)
 
 
-
+# quote_ticker, base_ticker = keys[quote], keys[base]
 bot.polling()
